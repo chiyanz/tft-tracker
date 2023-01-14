c75be1aec4db48f73278433537bc278b35e3cfb3
Johnny Zhou <jonathanqyz@gmail.com>
2023-01-13 20:03:07 -0500

    initalize project and types

---
+++ pages/profile.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import style from '../styles/Profile.module.scss'
+
+export default function Profile() {
+  return (
+    <h1>
+      Player profile page
+    </h1>
+  )
+}
---
+++ types/types.ts
@@ -0,0 +1,85 @@
+export interface Summoner {
+  id:            string;
+  accountId:     string;
+  puuid:         string;
+  name:          string;
+  profileIconId: number;
+  revisionDate:  number;
+  summonerLevel: number;
+}
+
+export interface GetSummoner {
+  summonerName: string;
+  result: Summoner;
+}
+
+export interface MatchHistory {
+  matchHistory: string[];
+}
+
+export interface GetMatchHistory {
+  puuid: string;
+  result: MatchHistory;
+}
+
+export interface MatchInfo {
+  metadata: Metadata;
+  info:     Info;
+}
+
+export interface Info {
+  game_datetime:     number;
+  game_length:       number;
+  game_version:      string;
+  participants:      Participant[];
+  queue_id:          number;
+  tft_game_type:     string;
+  tft_set_core_name: string;
+  tft_set_number:    number;
+}
+
+export interface Participant {
+  augments:                string[];
+  companion:               Companion;
+  gold_left:               number;
+  last_round:              number;
+  level:                   number;
+  placement:               number;
+  players_eliminated:      number;
+  puuid:                   string;
+  time_eliminated:         number;
+  total_damage_to_players: number;
+  traits:                  Trait[];
+  units:                   Unit[];
+}
+
+export interface Companion {
+  content_ID: string;
+  item_ID:    number;
+  skin_ID:    number;
+  species:    string;
+}
+
+export interface Trait {
+  name:         string;
+  num_units:    number;
+  style:        number;
+  tier_current: number;
+  tier_total:   number;
+}
+
+export interface Unit {
+  character_id: string;
+  itemNames:    string[];
+  items:        number[];
+  name:         string;
+  rarity:       number;
+  tier:         number;
+}
+
+export interface Metadata {
+  data_version: string;
+  match_id:     string;
+  participants: string[];
+}
+
