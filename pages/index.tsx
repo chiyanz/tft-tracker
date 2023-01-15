49f4ae6b2674b8a5a2c18c8d6659215da2c45d60
Johnny Zhou <jonathanqyz@gmail.com>
2023-01-15 11:09:37 -0500

    home page update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 const inter = Inter({ subsets: ['latin'] })
+const regions: string[] = ['NA', "EU", "EUNE", "EUW", "JP", "KR", "LAN", "LAS", "OCE", "TR", "RU", "PH", "SG", "TH"
+, "TW", "VN"]
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>TFT Tracker</title>
-        <meta name="description" content="TFT player stat tracker using Next.js" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-        <nav>
-          
-        </nav>
-      </header>
       <main>
-        <div>
-         <h1>TFT Tracker</h1>
+        <div className={styles['input-form']}>
+            <Dropdown className={styles['region-select']} options={regions} onChange={() => {}} value={regions[0]} placeholder="Select an option" />
+            <input className={styles['name-input']}/>
         </div>
       </main>
     </>
